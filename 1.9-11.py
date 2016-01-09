44661f383d69b8823c40ab3576f49e2dae817e04
thomastommy <ma18242312011@gmail.com>
2016-01-09 13:33:30 +0800

    1月9号，完成10-12练习。

---
+++ 1.9-11.py
@@ -0,0 +1,8 @@
+print("How old are you?",)
+age = input()
+print("How tall are you?",)
+height = input()
+print("How much do you weight?",)
+weight = input()
+
+print("So,you're %r old, %r tall and %r hervy." % (age,height,weight))
